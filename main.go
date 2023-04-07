a9e434c0efa260779a769aaf3ac493c5857f9038
rabilmadi <123709672+rabilmadi@users.noreply.github.com>
2023-04-07 23:08:55 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func areAnagrams(str1, str2 string) bool {
+	// Convert the input strings to lowercase and remove spaces
+	str1 = strings.ReplaceAll(strings.ToLower(str1), " ", "")
+	str2 = strings.ReplaceAll(strings.ToLower(str2), " ", "")
+
+	// Convert the input strings to slices of runes and sort them
+	str1Runes := []rune(str1)
+	str2Runes := []rune(str2)
+	sort.Slice(str1Runes, func(i, j int) bool { return str1Runes[i] < str1Runes[j] })
+	sort.Slice(str2Runes, func(i, j int) bool { return str2Runes[i] < str2Runes[j] })
+
+	// Convert the sorted rune slices back to strings
+	sortedStr1 := string(str1Runes)
+	sortedStr2 := string(str2Runes)
+
+	// Compare the sorted strings
+	return sortedStr1 == sortedStr2
+}
+
+func main() {
+	// Input two strings
+	var str1, str2 string
+	fmt.Print("Enter the first string: ")
+	fmt.Scanln(&str1)
+	fmt.Print("Enter the second string: ")
+	fmt.Scanln(&str2)
+
+	// Check if the strings are anagrams
+	if areAnagrams(str1, str2) {
+		fmt.Println("The two strings are anagrams of each other.")
+	} else {
+		fmt.Println("The two strings are not anagrams of each other.")
+	}
+}
